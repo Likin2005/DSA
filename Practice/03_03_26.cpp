e3aeb04ca89e46e67f7b1c8b6fcefa4281fb9b6f
Likin2005 <likinmb915>
2026-03-04 16:16:19 +0530

    update

---
+++ Practice/03_03_26.cpp
@@ -0,0 +1,131 @@
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+
+int max_consecutive_ones(vector<int> &num)
+{
+    if(num.empty()) return 0;
+    int count = 0, max_count = 0;
+    for(int i=0;i<num.size();i++)
+    {
+        if(num[i] == 1)
+        {
+            count++;
+            max_count = max(count, max_count);
+        }
+        else
+        {
+            count=0;
+        }
+    }
+    return max_count;
+}
+
+bool is_palindrome(string s)
+{
+    if(s.size() < 2) return true;
+    int left = 0, right = s.size()-1;
+    while(left < right)
+    {
+        if(s[left] != s[right])
+        {
+            return false;
+        }
+        left++, right--;
+    }
+    return true;
+}
+
+string reverse_string(string &s)
+{
+    if(s.size() < 2) return s;
+    int left = 0, right = s.size()-1;
+
+    while(left < right)
+    {
+        char x = s[left];
+        s[left] = s[right];
+        s[right] = x;
+        left++, right--;
+    }
+    return s;
+}
+
+bool valid_palindrome(string s)
+{
+    int left = 0, right = s.size()-1;
+    while(left < right)
+    {
+        if(!(isalnum(s[left])))
+        {
+            left++;
+            continue;
+        }
+        else if(!(isalnum(s[right])))
+        {
+            right--;
+            continue;
+        }
+        else
+        {
+            if((tolower(s[left]) != tolower(s[right])))
+            {
+                return false;
+            }
+        }
+        left++, right--;
+    }
+    return true;
+}
+
+void frequency(string s)
+{
+    if(s.empty()) return;
+    unordered_map<char, int> freq;
+    for(char i : s) freq[i]++;
+    for(auto i : freq) cout << i.first << " : " << i.second << " ";
+}
+
+char first_non_repeating(string s)
+{
+    map<char, int> seen;
+    for(char i : s)seen[i]++;
+    for(char i : s)
+    {
+        if(seen[i] == 1)
+        {
+            return i;
+        }
+    }
+    return ' ';
+}
+
+string remove_duplicate(string s)
+{
+    string res = "";
+    for(char i : s)
+    {
+        if(find(res.begin(), res.end(), i) == res.end())
+        {
+            res += i;
+        }
+    }
+    return res;
+}
+
+string toggle_case(string s)
+{
+    for(int i=0;i<s.size();i++)
+    {
+        if(islower(s[i])) s[i] = toupper(s[i]);
+        else s[i] = tolower(s[i]);
+    }
+    return s;
+}
+
+int main()
+{
+    string s = "Ai-Ml";
+    cout << toggle_case(s) << endl;
+    return 0;
+}
